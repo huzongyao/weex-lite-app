e0429cbe36d69164473960b0ecb523ab4db0a024
huzongyao <hzy3774@gmail.com>
2019-02-28 01:42:02 +0800

    get weather city

--- src/views/weather/WeatherMain.vue
+++ src/views/weather/WeatherMain.vue
@@ -9,13 +9,40 @@
 
   export default {
     name: "weather-main",
+    data() {
+      return {
+        urls: {
+          getCity: 'http://zhwnlapi.etouch.cn/Ecalender/api/city',
+          getWeather: 'http://zhwnlapi.etouch.cn/Ecalender/api/v2/weather?citykey=101280605',
+        },
+      }
+    },
     mounted() {
       if (geoLocation) {
         geoLocation.getLocation({}, res => {
-          this.$alert(res);
+          this.getCityInfo(res);
         });
       } else {
-
+        this.getCityInfo(null);
+      }
+    },
+    methods: {
+      getCityInfo(location) {
+        let la = '';
+        let lo = '';
+        if (location) {
+          la = location.latitude;
+          lo = location.longitude;
+        }
+        this.$get({
+          url: this.urls.getCity,
+          headers: {'user-agent': 'Android'},
+          data: {lon: lo, lat: la, type: 'gps'},
+        }).then(res => {
+          this.$alert(res)
+        }).catch(() => {
+          this.$alert('456')
+        })
       }
     }
   }
